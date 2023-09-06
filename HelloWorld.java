16cb44d900a3a9779e2bc6edb815aa88a0044108
LukeAlanWalters <143542485+LukeAlanWalters@users.noreply.github.com>
2023-09-05 20:51:14 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld{
 
 public static void main (String[] args) {
   PrintWriter pen = new PrintWriter(System.out, true);
-  pen.println ("Hello, GitHub from GitHub!");
+  pen.println ("Hello, GitHub from GitHub2!");
   pen.flush();
 } // main(String[])
 }
